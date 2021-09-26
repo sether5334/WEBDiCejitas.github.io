8943814fe1200f0ca29c17694bb6341a72d31ced
Sether <luis.valladares1988@gmail.com>
2021-09-26 18:46:19 -0500

    Add files via upload

--- js/mapa.js
+++ js/mapa.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded',function(){
- var map = L.map('map').setView([18.9264733,-99.2264237],17);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken:'pk.pk.eyJ1Ijoic2V0aGVyIiwiYSI6ImNrdDJoM245MjBvbWYydnA5MDhlamNlYnQifQ.Rki5i-iIKeyCDf0ha3dwdQ'
-}).addTo(mymap);
-
-});
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
